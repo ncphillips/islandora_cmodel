ba3cffa0ebb5448e32ef48cdb56f8f5397692eab
ncphillips <ncphillips.19@gmail.com>
2014-08-25 10:14:24 -0300

    Sparql

---
+++ islandora_cmodel.api.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @file
+ */
+
+/**
+ * @see islandora_cmodel_page()
+ */
+function hook_islandora_cmodel_page_alter(&$page, &$page_state){
+
+}
+
+/**
+ * @see islandora_cmodel_page()
+ */
+function hook_CMODEL_PID_cmodel_page_alter(&$page, &$page_state){
+
+}
